ac54a75e81dc6b2c3ce50fbf8a7a10985116960f
marannk <makissel@uvm.edu>
2017-04-13 16:44:04 -0400

    Created Owners Page

---
+++ owners.php
@@ -0,0 +1,29 @@
+<?php
+include "_includes/top.php";
+?>
+<body>
+    <?php
+    include "_includes/nav.php";
+    ?>
+    <h1> The Bernstein &amp; Co. Family </h1>
+    <p></p>
+    
+    <h2> Zach Bernstein </h2>
+    <p> insert info here </p>
+    
+    <h2> Olivia Hurd </h2>
+    <p> insert info here </p>
+    
+    <h2> Maria Kissel </h2>
+    <p> Maria Kissel was born on April 8, 1998 in Queens, New York. She has moved
+    between New York and Pennsylvania throughout her life. Her appreciation for 
+    travel and different cultures has allowed her to see a good chunk of the world. S
+    he has been to the Caribbean, Europe, Russia, and various parts of North America. 
+    Travel has also flourished her love for adventure. The need for adventure
+    caused her to end up at the University of Vermont where she now enjoys hiking, 
+    snowboarding, and eating good farm-to-table food. During her time at the 
+    University of Vermont, she has been able to take Lisa Dion's Into to Website
+    Development class where she discovered her love of CSS. With CSS, Maria has
+    been able to channel her inner designer and has given her the ability to create
+    cool websites. Her CSS skills were spotted by Bernstein & Co. where she is now
+    working. With CSS, the possibilities are endless. </p>
