e9de7cda39d4caadde67c554cfb672a7b5671fa5
marannk <makissel@uvm.edu>
2017-04-24 09:40:57 -0400

    Update owners.php

--- owners.php
+++ owners.php
@@ -15,18 +15,18 @@ include "_includes/top.php";
     <p> insert info here </p>
     
     <h2> Maria Kissel </h2>
-    <p> Maria Kissel was born on April 8, 1998 in Queens, New York. She has moved
-    between New York and Pennsylvania throughout her life. Her appreciation for 
-    travel and different cultures has allowed her to see a good chunk of the world. S
-    he has been to the Caribbean, Europe, Russia, and various parts of North America. 
-    Travel has also flourished her love for adventure. The need for adventure
-    caused her to end up at the University of Vermont where she now enjoys hiking, 
-    snowboarding, and eating good farm-to-table food. During her time at the 
-    University of Vermont, she has been able to take Lisa Dion's Into to Website
-    Development class where she discovered her love of CSS. With CSS, Maria has
-    been able to channel her inner designer and has given her the ability to create
-    cool websites. Her CSS skills were spotted by Bernstein &amp; Co. where she is now
-    working. With CSS, the possibilities are endless. </p>
+    <p> Maria Kissel was born on April 8, 1998 in Queens, New York; however, she has spent most 
+        of her life in Pennsylvania. Throughout her life, Maria has been able to travel growing
+        her appreciation for different cultures. She has been to the Caribbean, Europe, Russia, 
+        and various parts of North America. Through travel, her need for adventure has skyrocketed. 
+        In her freetime she enjoys snowboarding, hiking, and trying new things. The landscape surrounding
+        the University of Vermont had her absolutely in awe... that and the farm-to-table food motto. 
+        Upon deciding she wanted to be a Computer Science major, she found herself in Lisa Dion's Into to 
+        Website Development class where she discovered her love of CSS. Her reputation with CSS has now 
+        put the pressure in designing a sleek final project. With CSS, Maria has been able to utilize her 
+        sense of color combination to make websites look good. Now, she has the pleasure on tackling the CSS
+        for Bernstein &amp; Co. She hopes you enjoy all the time and effort the CSS took. 
+    </p>
 <?php
 include "_includes/footer.php";
     ?>
