36e3326875bc756fbf09c907026d93de7ed6b9f2
marannk <makissel@uvm.edu>
2017-04-24 09:49:14 -0400

    boston location

---
+++ boston.php
@@ -0,0 +1,28 @@
+<?php
+include "_includes/top.php";
+?>
+<body>
+     <?php
+    include "_includes/nav.php";
+    ?>
+    <div class="locations">
+    <h1> Boston, Massachusetts </h1>
+        <p> 75 Arlington St </p>
+        <p> Boston, MA 02116</p>
+        <p> Phone: (617)-474-8833 </p>
+    </div>
+    <div class="hours">
+    <h2>Hours</h2> 
+    <p> Monday-Friday: 11am - 11pm </p>
+    <p> Saturday-Sunday: 10am-11pm </p>
+    </div>
+    <div class ="reserveNow">
+    <p>
+      <a href="reservations.php" > Reserve now! </a>
+    </p>
+    </div>
+    <?php
+include "_includes/footer.php";
+    ?>
+
+
