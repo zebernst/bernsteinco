4bd2ca3f34ef1f0fff57654601ca1bcc10ab73e9
marannk <makissel@uvm.edu>
2017-04-24 09:52:00 -0400

    chicago location info

---
+++ chicago.php
@@ -0,0 +1,26 @@
+<?php
+include "_includes/top.php";
+?>
+<body>
+     <?php
+    include "_includes/nav.php";
+    ?>
+    <div class="location">
+    <h1> Chicago, Illinois </h1>
+        <p> 218 W Kinzie St </p>
+        <p> Chicago, IL 60654</p>
+        <p> Phone: (312)-956-0053 </p>
+    </div>
+    <div class="hours">
+    <h2>Hours</h2> 
+    <p> Monday-Friday: 11am - 11pm </p>
+    <p> Saturday-Sunday: 10am-11pm </p>
+    </div>
+    <div class ="reserveNow">
+    <p>
+      <a href="reservations.php" > Reserve now! </a>
+    </p>
+    </div>
+    <?php
+include "_includes/footer.php";
+    ?>
