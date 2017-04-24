52921a9ccca40f58d00a8bb72e6a453da2af44f7
marannk <makissel@uvm.edu>
2017-04-24 09:49:47 -0400

    Update burlington.php

--- burlington.php
+++ burlington.php
@@ -5,12 +5,13 @@ include "_includes/top.php";
      <?php
     include "_includes/nav.php";
     ?>
+     <div class="location">
     <h1> Burlington, Vermont </h1>
         <p> 7 Church St. </p>
         <p> Burlington, VT 05401</p>
         <p> Phone: (802)-576-8492 </p>
-    
-    <div class="burlyHrs">
+     </div>  
+    <div class="hours">
     <h2>Hours</h2> 
     <p> Monday-Friday: 11am - 11pm </p>
     <p> Saturday-Sunday: 10am-11pm </p>
