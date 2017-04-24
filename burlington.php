aad3574882f116ececbf644659bd5286bdb50126
marannk <makissel@uvm.edu>
2017-04-24 09:33:31 -0400

    burlington location info

---
+++ burlington.php
@@ -0,0 +1,15 @@
+<body>
+    <h1> Burlington, Vermont </h1>
+        <p> 7 Church St. </p>
+        <p> Burlington, VT 05401</p>
+        <p> Phone: (802)-576-8492 </p>
+    </h2>
+    <div class="burlyHrs">
+    <h2>Hours</h2> 
+    <p> Monday-Friday: 11am - 11pm </p>
+    <p> Saturday-Sunday: 10am-11pm </p>
+    </div>
+    <div class ="reserveNow"
+    <p><a href="reservations.php" > Reserve now! </a></p>
+    </div>
+</body>
