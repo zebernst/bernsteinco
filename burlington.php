c2e4aae1f67453450519fc07af7b075d52162629
Zach Bernstein <zacharybernstein42@me.com>
2017-04-25 13:49:31 -0400

    code formatting changes and general prettifying

--- burlington.php
+++ burlington.php
@@ -1,28 +1,19 @@
-<?php
-include "_includes/top.php";
-?>
-<body>
-     <?php
-    include "_includes/nav.php";
-    ?>
-     <div class="location">
-    <h1> Burlington, Vermont </h1>
-        <p> 7 Church St. </p>
-        <p> Burlington, VT 05401</p>
-        <p> Phone: (802)-576-8492 </p>
-     </div>  
-    <div class="hours">
-    <h2>Hours</h2> 
-    <p> Monday-Friday: 11am - 11pm </p>
-    <p> Saturday-Sunday: 10am-11pm </p>
-    </div>
-    <div class ="reserveNow">
+<?php include "_includes/top.php";?>
+<?php include "_includes/nav.php";?>
+<div class="location">
+    <h1>Burlington, Vermont</h1>
+    <p>7 Church St.</p>
+    <p>Burlington, VT 05401</p>
+    <p>Phone: (802)-576-8492</p>
+</div>
+<div class="hours">
+    <h2>Hours</h2>
+    <p>Monday-Friday: 11am - 11pm</p>
+    <p>Saturday-Sunday: 10am-11pm</p>
+</div>
+<div class="reserveNow">
     <p>
-      <a href="reservations.php" > Reserve now! </a>
+        <a href="reservations.php">Reserve now!</a>
     </p>
-    </div>
-    <?php
-include "_includes/footer.php";
-    ?>
-
-
+</div>
+<?php include "_includes/footer.php";?>
--- locations.php
+++ locations.php
@@ -1,36 +1,22 @@
-
-<?php
-include "_includes/top.php";
-?>
-<body>
-    <?php
-    include "_includes/nav.php";
-    ?>
-   
-<h1 id="locationPage"> Our Locations </h1>
-
+<?php include "_includes/top.php";?>
+<?php include "_includes/nav.php"; ?>
+<h1 id="locationPage">Our Locations</h1>
 <figure id="map">
     <img src="map.png" alt="Clickable map of restaurant locations" usemap="#map">
     <map name="map">
-        <area shape="rectangle"
-              alt="Burlington" 
-              coords="1693,252,1767,335" 
-              href="burlington.php>
+        <area shape="rectangle" alt="Burlington" coords="1693,252,1767,335" href="burlington.php>
 
-        <area shape="rectangle" 
-              alt="Boston" 
-              coords="1782,336,1857,412" 
-              href="Boston.php">
+        <area shape=" rectangle " 
+              alt="Boston " 
+              coords="1782,336,1857,412 " 
+              href="Boston.php ">
               
-       <area shape="rectangle" 
-              alt="Chicago" 
-              coords="1222,440,1297,521" 
-              href="Chicago.php">       
+       <area shape="rectangle " 
+              alt="Chicago " 
+              coords="1222,440,1297,521 " 
+              href="Chicago.php ">       
     </map>
-    <figcaption>Click one of our three locations for more information! </figcaption>
+    <figcaption>Click one of our three locations for more information!</figcaption>
 </figure>
 
-<?php
-include "_includes/footer.php";
-    ?>
-
+<?php include "_includes/footer.php ";?>
