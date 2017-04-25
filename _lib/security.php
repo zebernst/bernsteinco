a72ad8d07003ac95d4b5ac0382ef368f0fb942da
Zach Bernstein <zacharybernstein42@me.com>
2017-04-25 14:09:59 -0400

    added _lib folder

---
+++ _lib/security.php
@@ -0,0 +1,27 @@
+<?php
+print "\n<!--  BEGIN include security -->\n";
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// performs a simple security check to see if our page has submitted the form to itself
+function securityCheck($myFormURL = "") {
+    $debugThis = false;  // you have to spcefically want to test this
+    
+    $status = true; // start off thinking everything is good until a test fails
+    
+    // when it is a form page check to make sure it submitted to itself
+    if ($myFormURL != "") {
+        $fromPage = htmlentities($_SERVER['HTTP_REFERER'], ENT_QUOTES, "UTF-8");
+        
+        //remove http or https
+        $fromPage = preg_replace('#^https?:#', '', $fromPage);
+        
+        if ($debugThis)
+            print '<p>From: ' . $fromPage . ' should match your Url: ' . $myFormURL;
+        
+        if ($fromPage != $myFormURL) {
+            $status = false;
+        }
+    }
+    return $status;
+}
+print "\n<!--  END include security -->\n";
+?>
