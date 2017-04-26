d8532e3c36be0422e9516c37619ec59cc3b2ac1a
Zach Bernstein <zacharybernstein42@me.com>
2017-04-26 09:31:55 -0400

    added reservations.php

---
+++ reservations.php
@@ -0,0 +1,17 @@
+<?php include "_includes/top.php";?>
+<?php include "_includes/nav.php"; ?>
+<h1>Reservations</h1>
+<?php
+/*
+- Drop down for which location
+- Name of person (first and last)
+- Date/Time of Reservation
+- Phone number
+- Email
+- Number of people
+- Anything we should know (comments box)
+- Submit Button
+*/
+
+?>
+<?php include "_includes/footer.php ";?>
