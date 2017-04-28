61610ad58a9352e69b492bfcee746bb0d6dae1e7
marannk <makissel@uvm.edu>
2017-04-28 16:17:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 <?php include "_includes/nav.php";?>
 
 <article class="index">
-    <div id="homepage">
-<h1> Welcome! <img src="_media/homecollage.jpg" alt="photos of restaurant" align="right"> </h1>
+   <img src="_media/homecollage.jpg" alt="photos of restaurant"> 
+    <div id="home">
+<h1> Welcome!  </h1>
 <p>Bernstein &amp; Co. is your number one destination for classic steakhouse food in a modern environment. Here at Bernstein &amp; Co. we make it a priority to serve you the best meat on the market. We aim to provide a unique dining experience in our three amazing locations throughout the United States. All of our locations provide locally sourced food -- guaranteeing freshness and ensuring flavor.  </p>
 <p>Our menu is changed seasonally to reflect the produce in season. We partner with local farms to help enhance the access to their delicious food while helping the local community. The menu is crafted by our seasoned chefs who understand the ingredients and the best way to develop the flavors in the dishes. Our aim is to provide our guests with the best food out there and showing them just how good food can be.</p>
 <p>Our Vermont location provides an urban oasis nestled within the mountains. With its clean lines and view of Lake Champlain, it is the perfect destination for a night out or a celebration. There is always something to celebrate, and with our 100 person party room it has never been easier. In Vermont, we partner with local farmers who provide the freshest in season ingredients. Our partners in Vermont include Butternut Mountain Farm, Shelburne Farms, Misty Knoll Farm, Bread &amp; Butter Farm, Full Moon Farm, and the Shadow Cross Farm. Also, we provide a selection of alcohol from various breweries, vineyards and distilleries including Lost Nation Brewing, Zero Gravity Brewery, Stone Corral Brewery, Smugglers’ Notch Distillery, Mad River Distillers, Lincoln Peak Vineyard, and Shelburne Vineyard.</p>
 <p>Meanwhile, our city locations bring the outside in providing an escape from the busy life of the city. By bringing elements such as wood inside, you are transported into the mountains, even if the view outside is one of the concrete jungle. Our partners in Chicago and Boston include Parlee Farms, Verrill Farm, Didier Farms, and Heritage Prairie Farm all of which are dedicated into making your dining experience delicious and out of this world.</p>
 <p>Whether you are looking for a new dining experience, or to celebrate an event, Bernstein &amp; Co. offers it all! So come and try some of delicious dishes and let us serve you today!</p>
     </div>
+     
 <div id="feedback">
     <p><strong>Feedback from Real Customers</strong></p>
 
