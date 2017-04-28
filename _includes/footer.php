b1c55668acfc2b63fb5035dd430f1d8c2b3877e1
Zach Bernstein <zacharybernstein42@me.com>
2017-04-28 16:20:13 -0400

    added link to owners.php in footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,7 +1,7 @@
 		<!-- footer -->
         <footer>
 	<div id=footer>
-            <p>&copy; Bernstein &amp; Co. | About<!--Add link to owner --> </p>
+            <p>&copy; Bernstein &amp; Co. | <a href='owners.php'>About</a></p>
 		</div>
         </footer>
     </body>
