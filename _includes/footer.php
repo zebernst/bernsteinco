d29e7e80ee9808a4385e3f841b5f1023442b9796
marannk <makissel@uvm.edu>
2017-04-28 09:59:42 -0400

    Update footer.php

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,6 +1,8 @@
 		<!-- footer -->
         <footer>
+	<div id=footer>
             <p>&copy; Bernstein &amp; Co. | About<!--Add link to owner --> </p>
+		</div>
         </footer>
     </body>
 </html>
