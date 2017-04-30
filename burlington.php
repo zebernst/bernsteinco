b2a7ea8afe881dc0f906ee1c27ff0d1e385b80e2
marannk <makissel@uvm.edu>
2017-04-30 16:15:58 -0400

    Update burlington.php

--- burlington.php
+++ burlington.php
@@ -3,18 +3,22 @@
 <article class="actLocation">
 <div class="location">
     <h1>Burlington, Vermont</h1>
-    <p><a target="_blank" class="address" href="//www.google.com/maps/place/7+Church+St,+Burlington,+VT+05401">
+    <div class="locBox">
+        <p><a target="_blank" class="address" href="//www.google.com/maps/place/7+Church+St,+Burlington,+VT+05401">
     7 Church St.<br/>
     Burlington, VT 05401
     </a></p>
     <!-- TODO: put fontawesome phone icon in before telephone number -->
     <p><strong>Phone: </strong> <a class="phone" href="tel:802-576-8492">(802)-576-8492</a></p>
 </div>
+    </div>
 <div class="hours">
     <h2>Hours</h2>
+    <div class="locBox">
     <p><strong>Monday-Friday: </strong> 11am - 11pm</p>
     <p><strong>Saturday-Sunday: </strong>10am-11pm</p>
 </div>
+    </div>
 <div class="reserveNow">
     <p>
         <a href="reservations.php">Reserve now!</a>
