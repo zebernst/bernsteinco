d2b98eab128614192113addf62503d6b11ffed1c
marannk <makissel@uvm.edu>
2017-04-30 17:11:14 -0400

    Update chicago.php

--- chicago.php
+++ chicago.php
@@ -3,21 +3,24 @@
 <article class="actLocation">
 <div class="location">
     <h1>Chicago, Illinois</h1>
+     <div class="locBox">
     <p><a target="_blank" class="address" href="//www.google.com/maps/place/218+W+Kinzie+St,+Chicago,+IL+60654">
     218 W Kinzie St<br/>
     Chicago, IL 60654
     </a></p>
-    <p>Phone: <a class="phone" href="tel:312-956-0053">(312)-956-0053</a></p>
-</div>
+         <p><strong><u>Phone:</u></strong> <a class="phone" href="tel:312-956-0053">(312)-956-0053</a></p>
 <div class="hours">
     <h2>Hours</h2>
-    <p>Monday-Friday: 11am - 11pm</p>
-    <p>Saturday-Sunday: 10am-11pm</p>
+    <p><strong><u>Monday-Friday:</u> 11am - 11pm</strong></p>
+    <p><strong><u>Saturday-Sunday:</u> 10am-11pm </strong></p>
 </div>
+    </div>
+    </div>
 <div class="reserveNow">
     <p>
         <a href="reservations.php">Reserve now!</a>
     </p>
 </div>
+    <p id="extra"> <br/> </p>
 </article>
 <?php include "_includes/footer.php";?>
