33d9f07546e3580962540c81a885a9d432f79ebd
marannk <makissel@uvm.edu>
2017-04-30 14:51:42 -0400

    Update locations.php

--- locations.php
+++ locations.php
@@ -3,19 +3,7 @@
 <h1 id="locationPage">Our Locations</h1>
 <figure id="map">
     <img src="_media/map.png" alt="Clickable map of restaurant locations" usemap="#map">
-    <map name="map">
-        <area shape="rectangle" alt="Burlington" coords="1693,252,1767,335" href="burlington.php>
 
-        <area shape=" rectangle " 
-              alt="Boston " 
-              coords="1782,336,1857,412 " 
-              href="Boston.php ">
-              
-       <area shape="rectangle " 
-              alt="Chicago " 
-              coords="1222,440,1297,521 " 
-              href="Chicago.php ">       
-    </map>
     <figcaption>Click one of our three locations for more information!</figcaption>
 </figure>
 
