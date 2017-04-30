39c246094d71601a5782813e698fbeb089d08b82
marannk <makissel@uvm.edu>
2017-04-30 17:18:09 -0400

    Update burlington.php

--- burlington.php
+++ burlington.php
@@ -9,7 +9,7 @@
     Burlington, VT 05401
     </a></p>
     <!-- TODO: put fontawesome phone icon in before telephone number -->
-        <p><i class="fa fa-phone" aria-hidden="true"></i><strong><u>Phone:</u> </strong> <a class="phone" href="tel:802-576-8492">(802)-576-8492</a></p>
+        <p><strong><u><i class="fa fa-phone"></i>Phone:</u> </strong> <a class="phone" href="tel:802-576-8492">(802)-576-8492</a></p>
 <div class="hours">
     <h2>Hours</h2>
     <p><strong><u>Monday-Friday:</u> 11am - 11pm</strong></p>
