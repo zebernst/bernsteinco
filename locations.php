ce2ccbc4a09a8cc665b756a5828cb1000e673331
marannk <makissel@uvm.edu>
2017-04-30 15:08:07 -0400

    plz

--- locations.php
+++ locations.php
@@ -2,26 +2,26 @@
 <?php include "_includes/nav.php"; ?>
 
 <article class="locations">
-<h1>Our Locations</h1>
-<figure id="map">
-    <img src="_media/map.png" alt="Clickable map of restaurant locations" usemap="#map">
-    <map name="map">
-        <area shape="rectangle" 
+    <h1>Our Locations</h1>
+    <figure id="map">
+        <img src="_media/map.png" alt="Clickable map of restaurant locations" usemap="#map">
+        <map name="map">
+            <area shape="rectangle" 
               alt="Burlington" 
               coords="845,124,885,165" 
               href="burlington.php">
 
-        <area shape="rectangle" 
+            <area shape="rectangle" 
               alt="Boston " 
               coords="885,170,931,213" 
               href="boston.php ">
               
-       <area shape="rectangle " 
+        <area shape="rectangle " 
               alt="Chicago" 
               coords="603,216,657,267" 
               href="chicago.php ">       
-    </map>
-    <figcaption>Click one of our three locations for more information!</figcaption>
-</figure>
+        </map>
+        <figcaption>Click one of our three locations for more information!</figcaption>
+    </figure>
 </article>
 <?php include "_includes/footer.php"; ?>
