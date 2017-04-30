ce2fecdcfc0c0b55e826f3a5e3fa42dcdad98a6b
marannk <makissel@uvm.edu>
2017-04-30 17:08:57 -0400

    Update burlington.php

--- burlington.php
+++ burlington.php
@@ -12,8 +12,8 @@
         <p><strong><u>Phone:</u> </strong> <a class="phone" href="tel:802-576-8492">(802)-576-8492</a></p>
 <div class="hours">
     <h2>Hours</h2>
-    <p><strong><u>Monday-Friday:</u> </strong> <style color="white"> 11am - 11pm</style></p>
-    <p><strong><u>Saturday-Sunday: </u></strong><style color="white">10am-11pm</style></p>
+    <p><strong><u>Monday-Friday:</u> 11am - 11p</strong> m</p>
+    <p><strong><u>Saturday-Sunday: </u>10am-11pm</strong></p>
     </div>
     </div>
     </div>
