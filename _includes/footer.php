05719a114645646b004f0823057d13a05930f88f
marannk <makissel@uvm.edu>
2017-05-01 10:20:10 -0400

    Update footer.php

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,6 +1,7 @@
 		<!-- footer -->
         <footer>
 	<div id=footer>
+	<img src="_media/logo.png" alt="company logo">
             <p>&copy; Bernstein &amp; Co. | <a href='owners.php'> Owners</a></p>
 		</div>
         </footer>
