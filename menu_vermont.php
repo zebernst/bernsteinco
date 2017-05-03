03682085fb2770294dfb9f5e87d40f341c675523
marannk <makissel@uvm.edu>
2017-05-02 22:27:24 -0400

    Update menu_vermont.php

--- menu_vermont.php
+++ menu_vermont.php
@@ -1,5 +1,6 @@
 <?php include "_includes/top.php";?>
 <?php include "_includes/nav.php";?>
+<article class="actMenu">
 <?php
 	include "_lib/print_menu.php";
 	$vermont_menu_arr = json_decode(file_get_contents("_data/vermont_menu.json"), true);
@@ -7,4 +8,5 @@
 <div>
 	<?php if ($vermont_menu_arr) print_menu_dl($vermont_menu_arr); ?>
 </div>
+</article>
 <?php include "_includes/footer.php";?>
