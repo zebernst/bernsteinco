760b1f527aeec50807e623e7a497248df66fc495
marannk <makissel@uvm.edu>
2017-05-02 22:26:45 -0400

    Update menu_boston.php

--- menu_boston.php
+++ menu_boston.php
@@ -1,5 +1,6 @@
 <?php include "_includes/top.php";?>
 <?php include "_includes/nav.php";?>
+<article class="actMenu">
 <?php
 	include "_lib/print_menu.php";
 	$boston_menu_arr = json_decode(file_get_contents("_data/boston_menu.json"), true);
@@ -7,4 +8,5 @@
 <div>
 	<?php if ($boston_menu_arr) print_menu_dl($boston_menu_arr); ?>
 </div>
+</article>
 <?php include "_includes/footer.php";?>
