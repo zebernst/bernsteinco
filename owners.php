dc2e3bb130c8df9c442ad34c903fac9019e686f2
marannk <makissel@uvm.edu>
2017-05-02 22:37:02 -0400

    Update owners.php

--- owners.php
+++ owners.php
@@ -11,5 +11,6 @@
 
 <h2>Maria Kissel</h2>
 <p>Maria Kissel was born on April 8, 1998 in Queens, New York; however, she has spent most of her life in Pennsylvania. Throughout her life, Maria has been able to travel growing her appreciation for different cultures. She has been to the Caribbean, Europe, Russia, and various parts of North America. Through travel, her need for adventure has skyrocketed. In her freetime she enjoys snowboarding, hiking, and trying new things. The landscape surrounding the University of Vermont had her absolutely in awe... that and the farm-to-table food motto. Upon deciding she wanted to be a Computer Science major, she found herself in Lisa Dion's Into to Website Development class where she discovered her love of CSS. Her reputation with CSS has now put the pressure in designing a sleek final project. With CSS, Maria has been able to utilize her sense of color combination to make websites look good. Now, she has the pleasure on tackling the CSS for Bernstein &amp; Co. She hopes you enjoy all the time and effort the CSS took.</p>
+<p id="extra"> <br/> </p>
 </article>
 <?php include "_includes/footer.php";?>
