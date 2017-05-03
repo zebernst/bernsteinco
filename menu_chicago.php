0f680cf142bff7bf1f70fe1365b2c4029ae2efa9
marannk <makissel@uvm.edu>
2017-05-02 22:27:07 -0400

    Update menu_chicago.php

--- menu_chicago.php
+++ menu_chicago.php
@@ -1,5 +1,6 @@
 <?php include "_includes/top.php";?>
 <?php include "_includes/nav.php";?>
+<article class="actMenu">
 <?php
 	include "_lib/print_menu.php";
 	$chicago_menu_arr = json_decode(file_get_contents("_data/chicago_menu.json"), true);
@@ -7,4 +8,5 @@
 <div>
 	<?php if ($chicago_menu_arr) print_menu_dl($chicago_menu_arr); ?>
 </div>
+</article>
 <?php include "_includes/footer.php";?>
