8ed0cd96245d9f030b2e0fca9308f81ff5b8144d
Zach Bernstein <zacharybernstein42@me.com>
2017-05-04 17:55:58 -0400

    changed body id printing to html from php

--- _includes/top.php
+++ _includes/top.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php
-        // %%%%%%%%%%%%%%%%%%%%%%
-        // %%    path setup    %%
-        // %%%%%%%%%%%%%%%%%%%%%%
+	    // %%%%%%%%%%%%%%%%%%%%%%
+	    // %%    path setup    %%
+	    // %%%%%%%%%%%%%%%%%%%%%%
 
-        $domain  = "//";
-        $server  = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
-        $domain .= $server;
+	    $domain  = "//";
+	    $server  = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
+	    $domain .= $server;
 
-        $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
-        $path_parts = pathinfo($phpSelf);
+	    $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+	    $path_parts = pathinfo($phpSelf);
     ?>
 	<head>
 		<title>Bernstein &amp; Co. | <?php print ucfirst($path_parts['filename']); ?></title>
@@ -24,13 +24,14 @@
 		<link rel="stylesheet" href="_css/site.css" type="text/css" media="screen">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Pacifico|Raleway" rel="stylesheet">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		
         <?php
 		// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		// %%    include libraries    %%
 		// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
         print "\n" . "<!-- include libraries -->" . "\n";
-        require_once('_lib/security.php');
+        require_once('_lib/security.php'); // force require security functions
         
         if ($path_parts['filename'] == "reservations") {
             include "_lib/validation-functions.php";
@@ -39,9 +40,7 @@
 		?>
 	</head>
 	<!-- begin body -->
-    <?php print '    <body id="' . $path_parts['filename'] . '">'; ?>
+	<body id="<?php print $path_parts['filename']; ?>">
 		<header>
 			<p> Bernstein &amp; Co. </p>
 		</header>
-														 
-														 
